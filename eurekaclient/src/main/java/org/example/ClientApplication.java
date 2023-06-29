96e9e77c6ccfec6d40d5ac7cd6d2c1b301e2d064
yangyuankai <2820471430@qq.com>
2023-06-29 09:11:56 +0800

    feat: eureka client

---
+++ eurekaclient/src/main/java/org/example/ClientApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yuankai Yang
+ */
+@SpringBootApplication
+//@EnableEurekaClient
+public class ClientApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;/*
+ * 项目名称:    ${PROJECT_NAME}
+ * 创建人员:    ${USER}
+ * 创建时间:    ${YEAR}-${MONTH}-${DAY}
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
